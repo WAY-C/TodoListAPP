67c62457ff26d1588d6a56a62f3005ba4d7998b2
WAY-C <543486217@qq.com>
2021-09-10 15:36:12 +0800

    realized by event bus

---
+++ todo_list_EventBus/.history/src/App_20210909161630.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <TodoHeader />
+        <TodoList />
+        <TodoFooter />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader";
+import TodoList from "./components/TodoList";
+import TodoFooter from "./components/TodoFooter";
+
+export default {
+  name: "App",
+  components: [TodoHeader, TodoList, TodoFooter],
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todo_list_EventBus/.history/src/components/TodoItem_20210909182207.vue
@@ -0,0 +1,77 @@
+<template>
+  <li>
+    <label>
+      <input
+        type="checkbox"
+        :checked="todo.done"
+        @change="handleCheck(todo.id)"
+      />
+      <!-- 如下代码也可以实现，但是不推荐，因为修改了props -->
+      <!-- <input type="checkbox" v-model="todo.done" /> -->
+      <span>{{ todo.title }}</span>
+    </label>
+    <button class="btn btn-danger" @click="handleDelete(todo.id)">删除</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo", "checkTodo"],
+  methods: {
+    handleCheck(id) {
+      this.checkTodo(id);
+    },
+    handleDelete(id) {
+      if (confirm("真的要删除这件事吗？(OAO)!")) {
+        console.log(id);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+/*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+
+li:hover {
+  background-color: #ddd;
+}
+
+li:hover button {
+  display: block;
+}
+</style>
